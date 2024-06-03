36de8d13b7bc1c4774e946d7251525dada770520
ahsanmalik <malikmalku555@gmail.com>
2024-06-03 15:45:31 +0500

    Location

--- data/header.js
+++ data/header.js
@@ -2,7 +2,7 @@ const data = {
     image: "/images/profile.jpeg",
     name: "Ahsan Malik",
     jobTitle: "full stack Web Developer",
-    address: "Sahiwal, pakistan",
+    address: "Lahore, pakistan",
     experiance: "1 year experience",
     education: "BS Computer Science",
   };
